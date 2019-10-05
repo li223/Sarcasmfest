a5437eba1e0ba5d8687392534023d036f6e676a8
Liam Holt <27295070+li223@users.noreply.github.com>
2019-10-05 15:49:37 +0100

    Update Sarcasm.cs

--- Li223/Sarcasm.cs
+++ Li223/Sarcasm.cs
@@ -1,9 +1,12 @@
 static void Main(string[] _)
 {
   var text = "this does not align with our core values".ToArray();
-  for(var i = 1; i < text.Length; i++)
+
+  for (var i = 1; i < text.Length; i++)
   {
-    if (char.IsLower(text[i - 1])) text[i] = char.ToUpper(text[i]);
+    var cnt = 1;
+    if (char.IsWhiteSpace(text[i])) cnt++;
+    if (char.IsLower(text[i - cnt])) text[i] = char.ToUpper(text[i]);
     else text[i] = char.ToLower(text[i]);
   }
   Console.WriteLine(string.Join("", text));

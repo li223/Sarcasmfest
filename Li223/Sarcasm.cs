8ab1f9715f4b3e405ed2eae0e52e6f2febfbed42
Aadhyaan <72241281+Aadhyaan@users.noreply.github.com>
2020-10-14 10:07:34 +0530

    Update Sarcasm.cs

--- Li223/Sarcasm.cs
+++ Li223/Sarcasm.cs
@@ -1,13 +1,13 @@
 static void Main(string[] _)
 {
-  var text = "this does not align with our core values".ToArray();
+  var text = "This does not align with our core values".ToArray();
 
   for (var i = 1; i < text.Length; i++)
   {
     var cnt = 1;
     if (char.IsWhiteSpace(text[i])) cnt++;
-    if (char.IsLower(text[i - cnt])) text[i] = char.ToUpper(text[i]);
-    else text[i] = char.ToLower(text[i]);
+    if (char.IsLower(text[i - cnt])) text[i] = char.ToUpperCase(text[i]);
+    else text[i] = char.ToLowerCase(text[i]);
   }
   Console.WriteLine(string.Join("", text));
   Console.ReadLine();

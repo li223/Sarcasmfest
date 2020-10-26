a72c6c50130bee6f0184671fa67293efc748be20
Cory Metcalfe <corymetcalfe@gmail.com>
2020-10-26 18:08:33 -0500

    i moost ascii you to leave

---
+++ coryallegory/sarcasm.rb
@@ -0,0 +1,10 @@
+saywhat = "This repo does not align with our core values"
+
+yesssyesss = ''
+DOIT = 32
+or_something = true
+saywhat.upcase.each_char do |c|
+    yesssyesss << (c.ord.between?(65,90) ? (c.ord+(should_probably = DOIT * ((or_something ^= 'already') && 1 || 0))).chr : c)
+end
+
+puts yesssyesss
